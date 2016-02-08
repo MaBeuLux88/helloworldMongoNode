af132c2dcdcad111082c5c7ee6d05d3d829675d9
Maxime Beugnet <maxime.beugnet@zenika.com>
2016-02-08 15:35:47 +0100

    update MongoDB start script to handle the folders and the log correctly

--- mongo/start.sh
+++ mongo/start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-mongod --dbpath data --logpath log/mongod.log --fork
+
+# Make folders if they don't exist yet
+if [ ! -d "./data" ]; then
+  mkdir data
+fi
+if [ ! -d "./log" ]; then
+  mkdir log
+fi
+
+# Start MongoDB single node instance
+mongod --dbpath data --logpath log/mongod.log --logappend --fork
